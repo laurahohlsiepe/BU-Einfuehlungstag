db60ff2abb00969ce363724fb8dd4355cd4f3ec1
laurahohlsiepe <l.hohlsiepe@gmail.com>
2022-05-18 00:05:01 +0200

    feat styling

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,27 +1,23 @@
 <template>
   <div class="hello box-border mx-10 font-sans">
-    <h1 class="text-2xl font-bold text-center m-5">Corona-Dashboard</h1>
-    <p class="text-center m-5">Anzeige Land,Datum</p>
+    <h1 class="text-4xl font-bold text-center m-7">Corona-Dashboard</h1>
+    <p class="text-center m-5">{{ cases.country }} am {{ updated }}</p>
 
-    <div class="border-2 border-amber-800">
+    <div class="border-2">
       <table class="table-fixed w-full text-center">
         <thead>
           <tr>
-            <th class="bg-blue-500 py-5">
+            <th class="bg-slate-400 py-5">
               Zusammenfassung der aktuellen Corona-Inzidenz-Zahlen
             </th>
 
-            <th class="bg-blue-500 py-5">Liste der letzten Fallzahlen</th>
+            <th class="bg-slate-400 py-5">Liste der letzten Fallzahlen</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>{{ cases.confirmed }}</td>
-            <td>{{ vaccines.people_vaccinated }}</td>
-          </tr>
-          <tr>
-            <td>test</td>
-            <td>test</td>
+            <td class="py-8 text-lg">{{ cases.confirmed }}</td>
+            <td class="py-8 text-lg">{{ vaccines.people_vaccinated }}</td>
           </tr>
         </tbody>
       </table>
@@ -42,20 +38,21 @@ export default {
     return {
       cases: {},
       vaccines: {},
+      updated: {},
     };
   },
   methods: {
     loadData() {
-      console.log("data");
+      /* console.log("data"); */
       fetch("https://covid-api.mmediagroup.fr/v1/cases")
         .then((response) => {
           return response.json();
         })
         .then((data) => {
           this.cases = data.Germany.All;
-          console.log(data.Germany.All);
         });
     },
+
     loadVaccines() {
       fetch("https://covid-api.mmediagroup.fr/v1/vaccines")
         .then((response) => {
@@ -65,10 +62,33 @@ export default {
           this.vaccines = data.Germany.All;
         });
     },
+
+    loadCountry() {
+      fetch("https://covid-api.mmediagroup.fr/v1/cases")
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          this.country = data.Germany.All.country;
+        });
+    },
+
+    loadDate() {
+      fetch("https://covid-api.mmediagroup.fr/v1/cases")
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          this.updated = data.Germany.Unknown.updated;
+          console.log(data.Germany.Unknown.updated);
+        });
+    },
   },
   mounted() {
     this.loadData();
     this.loadVaccines();
+    this.loadDate();
+    this.loadCountry();
   },
 };
 </script>
